e0d048922444827aa9009b7f2e9c4b2d99733a09
KosarAnsari <kosaransari55@gmail.com>
2025-09-03 18:27:33 +0530

    Create User.jsx

---
+++ User.jsx
@@ -0,0 +1,44 @@
+function User() {
+  return (
+    <>
+      <div>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+        <p> user name is anil sidhu</p>
+      </div>
+    </>
+  );
+}
+export default User;
